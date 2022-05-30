4e1b3834ef9cfa889dcdb68ee4af34973a975acf
Vitaliy Ageev <vitaly.ageev@rktv.ru>
2022-05-30 18:19:20 +0500

    Fixation

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -1,6 +1,10 @@
 import React, { FunctionComponent, useEffect, useRef, useState } from 'react'
 import Cross from '../UI/Icons/Cross/Cross'
 import classes from './FAQ.module.scss'
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
 
 interface IFAQItems {
     id: number,
@@ -13,35 +17,49 @@ interface IFAQ {
 }
 
 const FAQ: FunctionComponent<IFAQ> = ({ IFAQItems }) => {
-    const [currItem, setCurrItem] = useState(false);
-    const clickItem = (e: any) => {
-        setCurrItem(true);
+    const [currItem, setCurrItem] = useState(true);
 
+    let rootClasses = [classes.faq_item]
+    const clickItem = () => {
+        setCurrItem(true)
         if (currItem) {
             setCurrItem(false)
-            e.currentTarget.className = classes.faq_item
+            rootClasses = [classes.faq_item]
+
         } else {
             setCurrItem(true)
-            e.currentTarget.className += ' ' + classes.active
+            rootClasses.push(classes.active)
         }
     }
 
     return (
         <div className={classes.faq}>
             {IFAQItems.map(item =>
-                <div key={item.id} className={classes.faq_item} onClick={(e) => clickItem(e)}>
-                    <div className={classes.faq_item_title_block}>
-                        <span className={classes.faq_item_title}>
-                            {item.title}
-                        </span>
-                        <Cross />
+                <Accordion square className={rootClasses.join(' ')} onChange={() => setCurrItem(true)}
+                    style={currItem ? { border: `2px solid #000 !important` } : {}}>
+                    <div className={rootClasses.join(' ')} style={currItem ? { border: `2px solid #000 !important` } : {}}>
+                        <AccordionSummary>
+                            <div className={classes.faq_item_title_block} >
+                                <span className={classes.faq_item_title}>
+                                    {item.title}
+                                </span>
+                                <Cross />
+                            </div>
+                        </AccordionSummary>
                     </div>
-                    <div className={classes.faq_item_description}>
-                        <p>
-                            {item.description}
-                        </p>
-                    </div>
-                </div>
+
+                    <AccordionDetails className={classes.faq_item_description}>
+                        <Typography >
+                            <div >
+                                <p>
+                                    {item.description}
+                                </p>
+                            </div>
+                        </Typography>
+
+                    </AccordionDetails>
+
+                </Accordion>
             )}
         </div>
     )
